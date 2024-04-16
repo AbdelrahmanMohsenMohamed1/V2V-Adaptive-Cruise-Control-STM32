6de5d133a164822d3efb78756ee69ee04449d5e6
Tarek Alaa <103005836+Tarek191020@users.noreply.github.com>
2024-04-16 04:51:27 +0200

    LCD_update

--- Core/Inc/LCD_config.h
+++ Core/Inc/LCD_config.h
@@ -11,18 +11,19 @@
 #define LCD_CONFIG_H_
 
 // Define the ports used for LCD data and control signals
-#define LCD_Data_Port GPIOA
-#define LCD_Ctrl_Port GPIOA
+#define LCD_Data_Port 			GPIOA
+#define LCD_Ctrl_Port 			GPIOA
 
 // Define the pins used for LCD control signals
-#define LCD_RS_Pin	0 // Register Select pin
-#define LCD_EN_Pin	1 // Enable pin
+#define LCD_RS_Pin				GPIO_PIN_0 // Register Select pin
+#define LCD_EN_Pin				GPIO_PIN_1 // Enable pin
 
 // Define the pins used for LCD data signals (4-bit mode)
-#define LCD_Pin4   2 // Data pin 4
-#define LCD_Pin5   3 // Data pin 5
-#define LCD_Pin6   4 // Data pin 6
-#define LCD_Pin7   5 // Data pin 7
+#define LCD_Pin4   				GPIO_PIN_2 // Data pin 4
+#define LCD_Pin5   				GPIO_PIN_3 // Data pin 5
+#define LCD_Pin6   				GPIO_PIN_4 // Data pin 6
+#define LCD_Pin7   				GPIO_PIN_5 // Data pin 7
+
 
 
 #endif /* LCD_CONFIG_H_ */
