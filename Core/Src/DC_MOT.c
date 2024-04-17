62076a93e3f2da3a19204d2fecea993a4999488c
roqaia <109293965+RoqaiaWagih@users.noreply.github.com>
2024-04-17 02:10:58 +0200

    Update DC_MOT.c

--- Core/Src/DC_MOT.c
+++ Core/Src/DC_MOT.c
@@ -8,86 +8,123 @@
 
 #include "DC_MOT.h"
 
-
-void DC_Motor_Init(void)
+void DC_Motor1_Start(void)
 {
-	HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_1); //start PWM on TIM3 channel 1
-	HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_1); //start PWM on TIM4 channel 1
-	TIM3->CCR1 = 0;
-	TIM4->CCR1 = 0;
+
+	   HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_1);
+	   HAL_GPIO_WritePin(GPIOA, ENA_PIN_6, GPIO_PIN_SET);  //EN1 PIN ENABLE HIGH
+}
+
+void DC_Motor2_Start(void)
+{
+
+	HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_1);
+	HAL_GPIO_WritePin(GPIOB, ENA_PIN_6, GPIO_PIN_SET);  //EN2 PIN ENABLE HIGH
+}
+void DC_Motor1_SetSpeed(uint8_t dutyCycle)
+{
+	/*
+	 * the eqution is
+	 * percentage=(inputValue-mininput)/(maxinput-mininput)
+	 * outputvalue=percentage*(maxoutput-minoutput)+minoutput
+
+	uint16_t Duty_Cycle_Percentage=dutyCycle/100;
+	uint16_t Mapping_OutputValue=Duty_Cycle_Percentage*(255);*/
+
+	//uint16_t Duty_Cycle_Percentage =(dutyCycle*65535)/100;
+
+//This register is used to store the capture value when the TIM3 channel 1 is configured in input capture mode.
+	TIM3->CCR1 = dutyCycle;
+}
+void DC_Motor2_SetSpeed(uint8_t dutyCycle)
+{
+	/*
+	 * the eqution is
+	 * percentage=(inputValue-mininput)/(maxinput-mininput)
+	 * outputvalue=percentage*(maxoutput-minoutput)+minoutput
+
+	uint16_t Duty_Cycle_Percentage=dutyCycle/100;
+	uint16_t Mapping_OutputValue=Duty_Cycle_Percentage*(255);*/
+
+	uint16_t Duty_Cycle_Percentage =(dutyCycle*255)/100;
+
+	TIM4->CCR1 = Duty_Cycle_Percentage;
 }
 void DC_Motor_SetSpeed(uint8_t dutyCycle)
 {
-	TIM3->CCR1 = dutyCycle;
-	TIM4->CCR1 = dutyCycle;
+	uint16_t Duty_Cycle_Percentage =(dutyCycle*99)/100;
+	TIM3->CCR1 = Duty_Cycle_Percentage;
+	TIM4->CCR1 = Duty_Cycle_Percentage;
 }
 void Decrease_speed_Gradually(uint8_t speed)
 {
-	for(uint8_t i=speed;i>=0;i--)
+	uint16_t Duty_Cycle_Percentage =(speed*99)/100;
+
+	for(uint16_t i=Duty_Cycle_Percentage;i>=0;i--)
 	{
 		DC_Motor_SetSpeed(i);
 		HAL_Delay(250);
+
 	}
 }
 void increase_speed_Gradually(uint8_t speed)
 {
-	uint8_t i=0;
-	for(i=0; i<=speed ;i++)
-	{
-		DC_Motor_SetSpeed(i);
-		HAL_Delay(200);
-	}
+	uint16_t Duty_Cycle_Percentage =(speed*99)/100;
+	for(uint16_t i=0;i<=Duty_Cycle_Percentage;i++)
+		{
+			DC_Motor_SetSpeed(i);
+			HAL_Delay(250);
+		}
 }
 
 void DC_Motor_SetDirection(uint8_t direction)
 {
-	switch (direction)
-	{
-	case 1: //Forward
-		HAL_GPIO_WritePin(GPIOB, IN1_PIN_5, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GPIOA, IN2_PIN_15, GPIO_PIN_RESET);
-
-		HAL_GPIO_WritePin(GPIOB, IN4_PIN_4, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GPIOB, IN3_PIN_3, GPIO_PIN_RESET);
-
-		break;
-	case 2: // Backward
-
-		HAL_GPIO_WritePin(GPIOA, IN2_PIN_15, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GPIOB, IN3_PIN_3, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GPIOB, IN1_PIN_5, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOB, IN4_PIN_4, GPIO_PIN_RESET);
-
-
-		break;
-	case 3: // Left
-		HAL_GPIO_WritePin(GPIOB, IN3_PIN_3, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GPIOB, IN4_PIN_4, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOB, IN1_PIN_5, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOA, IN2_PIN_15, GPIO_PIN_RESET);
-
-		break;
-	case 4: // Right
-
-		HAL_GPIO_WritePin(GPIOB, IN3_PIN_3, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOB, IN4_PIN_4, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(GPIOB, IN1_PIN_5, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(GPIOA, IN2_PIN_15, GPIO_PIN_RESET);
-
-		break;
-	default:
-		break;
-
-	}
-}
-
-
-void DC_Motor_STOP(void)
+switch (direction)
 {
-	TIM3->CCR1 = 0;
-	TIM4->CCR1 = 0;
+           case 1: //Forward
+        	   HAL_GPIO_WritePin(GPIOB, IN1_PIN_5, GPIO_PIN_SET);
+        	   HAL_GPIO_WritePin(GPIOB, IN4_PIN_4, GPIO_PIN_SET);
+        	   HAL_GPIO_WritePin(GPIOA, IN2_PIN_15, GPIO_PIN_RESET);
+        	   HAL_GPIO_WritePin(GPIOB, IN3_PIN_3, GPIO_PIN_RESET);
+
+            break;
+        case 2: // Backward
+
+        	 HAL_GPIO_WritePin(GPIOA, IN2_PIN_15, GPIO_PIN_SET);
+        	 HAL_GPIO_WritePin(GPIOB, IN3_PIN_3, GPIO_PIN_SET);
+        	 HAL_GPIO_WritePin(GPIOB, IN1_PIN_5, GPIO_PIN_RESET);
+        	 HAL_GPIO_WritePin(GPIOB, IN4_PIN_4, GPIO_PIN_RESET);
+
+
+            break;
+        case 3: // Left
+        	 HAL_GPIO_WritePin(GPIOB, IN3_PIN_3, GPIO_PIN_SET);
+        	HAL_GPIO_WritePin(GPIOB, IN4_PIN_4, GPIO_PIN_RESET);
+        	 HAL_GPIO_WritePin(GPIOB, IN1_PIN_5, GPIO_PIN_RESET);
+        	 HAL_GPIO_WritePin(GPIOA, IN2_PIN_15, GPIO_PIN_RESET);
+
+            break;
+        case 4: // Right
+
+        	   HAL_GPIO_WritePin(GPIOB, IN3_PIN_3, GPIO_PIN_RESET);
+        	   HAL_GPIO_WritePin(GPIOB, IN4_PIN_4, GPIO_PIN_SET);
+        	   HAL_GPIO_WritePin(GPIOB, IN1_PIN_5, GPIO_PIN_RESET);
+        	   HAL_GPIO_WritePin(GPIOA, IN2_PIN_15, GPIO_PIN_RESET);
+
+            break;
+        default:
+            break;
+
+}
+}
+void DC_Motor1_STOP(void)
+{
 	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_1); // Close TIM3
-	//HAL_GPIO_WritePin(GPIOA, ENA_PIN_6, GPIO_PIN_RESET);  //EN1 PIN ENABLE LOW
+	 HAL_GPIO_WritePin(GPIOA, ENA_PIN_6, GPIO_PIN_RESET);  //EN1 PIN ENABLE LOW
+}
+void DC_Motor2_STOP(void)
+{
+
 	HAL_TIM_PWM_Stop(&htim4, TIM_CHANNEL_1);  //Close TIM4
-	//HAL_GPIO_WritePin(GPIOB, ENA_PIN_6, GPIO_PIN_RESET);  //EN1 PIN ENABLE LOW
+	 HAL_GPIO_WritePin(GPIOB, ENA_PIN_6, GPIO_PIN_RESET);  //EN1 PIN ENABLE LOW
 }
